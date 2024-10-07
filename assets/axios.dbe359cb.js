09decc18ec05deeb7a081dd587ae165cf95e8b75
luiznogueira <luizetn@gmail.com>
2024-10-07 17:50:59 -0300

    v2024.1007.1735

--- assets/axios.dbe359cb.js
+++
@@ -1,1 +0,0 @@
-import{b as s,a as o}from"./index.24dd679d.js";import{a as e}from"./index.24dd679d.js";var r=s(({app:a})=>{a.config.globalProperties.$axios=o,globalThis.$axios=o});export{e as axios,r as default};
