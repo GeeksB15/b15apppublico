c42622a54f0a6864ec37510bc20a22580107969a
Carlos Neves Lenz Cesar <cnlenzc@gmail.com>
2024-10-17 15:53:44 -0300

    2024.1017.1523

--- assets/axios.c0899d1c.js
+++
@@ -1,1 +0,0 @@
-import{b as s,a as o}from"./index.fec41db1.js";import{a as e}from"./index.fec41db1.js";var r=s(({app:a})=>{a.config.globalProperties.$axios=o,globalThis.$axios=o});export{e as axios,r as default};
---
+++ assets/g.58995234.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.6c26b0ec.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
