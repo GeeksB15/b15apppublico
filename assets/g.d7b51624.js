0654e02326891a6ffb2238337614cc2d9448e8b2
Carlos Neves Lenz Cesar <cnlenzc@gmail.com>
2024-11-06 09:32:01 -0300

    2024.1106.0926

---
+++ assets/g.d7b51624.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.95a3b5b2.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
---
+++ assets/utils.e70eb4cc.js
@@ -0,0 +1,1 @@
+import{b as s,aP as l}from"./index.95a3b5b2.js";var a=s(({app:o})=>{o.config.globalProperties.$utils=l,globalThis.$utils=l});export{a as default};
