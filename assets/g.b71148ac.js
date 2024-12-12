22e1c736b04b8ed30e610978895d69f6f879c334
Carlos Lenz <cnlenzc@gmail.com>
2024-12-12 09:47:13 -0300

    2024.1212.0940

---
+++ assets/g.b71148ac.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.8ba5bd75.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
---
+++ assets/router.cf27ae4d.js
@@ -0,0 +1,1 @@
+import{b as t}from"./index.8ba5bd75.js";var e=t(({app:r,router:o})=>{r.config.globalProperties.$router=o,globalThis.$router=o});export{e as default};
