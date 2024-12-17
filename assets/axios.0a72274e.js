0690998f8062575fd468787ef7c7c86c18e1faea
luiznogueira <luizetn@gmail.com>
2024-12-17 08:14:07 -0300

    2024.1217.0810

---
+++ assets/axios.0a72274e.js
@@ -0,0 +1,1 @@
+import{b as s,a as o}from"./index.84ee189e.js";import{a as e}from"./index.84ee189e.js";var r=s(({app:a})=>{a.config.globalProperties.$axios=o,globalThis.$axios=o});export{e as axios,r as default};
--- assets/g.b71148ac.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./index.8ba5bd75.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
