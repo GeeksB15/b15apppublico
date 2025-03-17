83549cde20a4470cc7d66444c943169067acd066
Carlos Lenz <cnlenzc@gmail.com>
2025-03-17 15:05:26 -0300

    2025.0317.1503

--- assets/axios.c323e469.js
+++
@@ -1,1 +0,0 @@
-import{b as s,a as o}from"./index.43a5bc4f.js";import{a as e}from"./index.43a5bc4f.js";var r=s(({app:a})=>{a.config.globalProperties.$axios=o,globalThis.$axios=o});export{e as axios,r as default};
--- assets/router.d4f2d506.js
+++
@@ -1,1 +0,0 @@
-import{b as t}from"./index.43a5bc4f.js";var e=t(({app:r,router:o})=>{r.config.globalProperties.$router=o,globalThis.$router=o});export{e as default};
