66dda033de97e9dd41349872972c0eb260daa627
luiznogueira <luizetn@gmail.com>
2025-05-27 16:25:12 -0300

    2025.0527.1621

--- assets/axios.0fafef58.js
+++
@@ -1,1 +0,0 @@
-import{b as s,a as o}from"./index.9776401b.js";import{a as e}from"./index.9776401b.js";var r=s(({app:a})=>{a.config.globalProperties.$axios=o,globalThis.$axios=o});export{e as axios,r as default};
