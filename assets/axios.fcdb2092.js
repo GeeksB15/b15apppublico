ca8eeae321adae4df81d5de680802ccdd64e1221
Carlos Lenz <cnlenzc@gmail.com>
2025-06-24 09:47:39 -0300

    2025.0624.0944

---
+++ assets/axios.fcdb2092.js
@@ -0,0 +1,1 @@
+import{b as s,a as o}from"./index.e5cbf892.js";import{a as e}from"./index.e5cbf892.js";var r=s(({app:a})=>{a.config.globalProperties.$axios=o,globalThis.$axios=o});export{e as axios,r as default};
