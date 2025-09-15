167e2806daf1ff285c6c0d69d58b8b8a6151ebd5
Carlos Lenz <cnlenzc@gmail.com>
2025-09-15 11:23:24 -0300

    2025.0915.1121

---
+++ assets/axios.970d00f6.js
@@ -0,0 +1,1 @@
+import{b as s,a as o}from"./index.b842a72c.js";import{a as e}from"./index.b842a72c.js";var r=s(({app:a})=>{a.config.globalProperties.$axios=o,globalThis.$axios=o});export{e as axios,r as default};
--- assets/g.e4534122.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./index.73a12394.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
