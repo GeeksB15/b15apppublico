74dfe915e8db03b38ccbff41f7ea913deb91bedf
Carlos Lenz <cnlenzc@gmail.com>
2025-09-18 10:25:47 -0300

    2025.0918.1023

---
+++ assets/axios.070297a3.js
@@ -0,0 +1,1 @@
+import{b as s,a as o}from"./index.fb978860.js";import{a as e}from"./index.fb978860.js";var r=s(({app:a})=>{a.config.globalProperties.$axios=o,globalThis.$axios=o});export{e as axios,r as default};
