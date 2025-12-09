1054fbb3001025c4a614ed2e583a01555ed42df7
luiznogueira <luizetn@gmail.com>
2025-12-09 11:18:21 -0300

    2025.1209.1116

--- assets/g.f64d5830.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./index.ca69ba74.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
---
+++ assets/g.f9478b34.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.d204a15d.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
