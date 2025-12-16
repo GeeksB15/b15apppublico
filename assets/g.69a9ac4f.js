ac2cbdf4860877bef1730c7a0be6f59c4119a1e4
luiznogueira <luizetn@gmail.com>
2025-12-16 15:09:13 -0300

    2025.1216.1507

---
+++ assets/g.69a9ac4f.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.03a0bbd8.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
--- assets/router.4d5181da.js
+++
@@ -1,1 +0,0 @@
-import{b as t}from"./index.d204a15d.js";var e=t(({app:r,router:o})=>{r.config.globalProperties.$router=o,globalThis.$router=o});export{e as default};
