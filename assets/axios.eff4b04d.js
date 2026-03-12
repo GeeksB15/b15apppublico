da4839c7166935dee26d2f778ec8912b0de57d2e
Carlos Lenz <cnlenzc@gmail.com>
2026-03-12 10:47:10 -0300

    2026.0312.1044

--- assets/axios.eff4b04d.js
+++
@@ -1,1 +0,0 @@
-import{b as s,a as o}from"./index.6c348ebb.js";import{a as e}from"./index.6c348ebb.js";var r=s(({app:a})=>{a.config.globalProperties.$axios=o,globalThis.$axios=o});export{e as axios,r as default};
---
+++ assets/g.8597657c.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.22e6eb90.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
