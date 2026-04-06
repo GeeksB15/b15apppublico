515d6f0dee863cbcef153d91f68e5cffc6bad299
Carlos Lenz <cnlenzc@gmail.com>
2026-04-06 16:42:56 -0300

    Revert "2026.0406.1444"
    This reverts commit 069fbd42e8ce06d613c10def53834486620203ae.

---
+++ assets/g.1946bce7.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.fcc0eb09.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
---
+++ assets/utils.e3c12631.js
@@ -0,0 +1,1 @@
+import{b as s,aH as l}from"./index.fcc0eb09.js";var a=s(({app:o})=>{o.config.globalProperties.$utils=l,globalThis.$utils=l});export{a as default};
