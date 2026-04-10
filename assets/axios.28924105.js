3fbf88d1d80c5cb5c17ccdcc40cfe94537879df2
luiznogueira <luizetn@gmail.com>
2026-04-10 15:57:26 -0300

    2026.0410.1556

---
+++ assets/axios.28924105.js
@@ -0,0 +1,1 @@
+import{b as s,a as o}from"./index.66caf0af.js";import{a as e}from"./index.66caf0af.js";var r=s(({app:a})=>{a.config.globalProperties.$axios=o,globalThis.$axios=o});export{e as axios,r as default};
---
+++ assets/lodash.71eef1e5.js
@@ -0,0 +1,1 @@
+import{b as l,aC as o}from"./index.66caf0af.js";var e=l(({app:a})=>{a.config.globalProperties.$lodash=o,globalThis.$lodash=o,a.config.globalProperties.$get=o.get,globalThis.$get=o.get});export{e as default};
