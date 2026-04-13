2615e0faf6513fc992925469b32ae1533220bde3
luiznogueira <luizetn@gmail.com>
2026-04-13 15:27:24 -0300

    2026.0413.1526

---
+++ assets/axios.c842aeb3.js
@@ -0,0 +1,1 @@
+import{b as s,a as o}from"./index.5e1f5660.js";import{a as e}from"./index.5e1f5660.js";var r=s(({app:a})=>{a.config.globalProperties.$axios=o,globalThis.$axios=o});export{e as axios,r as default};
--- assets/lodash.71eef1e5.js
+++
@@ -1,1 +0,0 @@
-import{b as l,aC as o}from"./index.66caf0af.js";var e=l(({app:a})=>{a.config.globalProperties.$lodash=o,globalThis.$lodash=o,a.config.globalProperties.$get=o.get,globalThis.$get=o.get});export{e as default};
