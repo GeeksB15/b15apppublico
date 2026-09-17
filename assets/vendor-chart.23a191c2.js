7a809c3cecb6daa2d876cc00f500dfea739e4b3e
Carlos Lenz <cnlenzc@gmail.com>
2026-09-17 10:31:16 -0300

    2026.0917.1027

--- assets/vendor-chart.23a191c2.js
+++ assets/vendor-chart.23a191c2.js
@@ -1,4 +1,4 @@
-import{c as Xi}from"./index.30a2d253.js";var Yr={exports:{}};/*!
+import{c as Xi}from"./index.5af36fb7.js";var Yr={exports:{}};/*!
  * Chart.js v2.9.4
  * https://www.chartjs.org
  * (c) 2020 Chart.js Contributors
