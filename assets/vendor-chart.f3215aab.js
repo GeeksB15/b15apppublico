8d7bb276c2833d8df3a43821c914a3851360b427
luiznogueira <luizetn@gmail.com>
2026-09-22 16:30:11 -0300

    2026.0922.1629

--- assets/vendor-chart.f3215aab.js
+++ assets/vendor-chart.f3215aab.js
@@ -1,4 +1,4 @@
-import{c as Xi}from"./index.65fb179b.js";var Yr={exports:{}};/*!
+import{c as Xi}from"./index.8dbb9f68.js";var Yr={exports:{}};/*!
  * Chart.js v2.9.4
  * https://www.chartjs.org
  * (c) 2020 Chart.js Contributors
